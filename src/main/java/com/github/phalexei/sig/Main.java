5c79fa076c80ae77b7a505996b1a35f6169b28c4
alexis <alexis.chretienne@gmail.com>
2015-01-15 18:44:00 +0100

    added graphical UI (provided). Ready to dev ...

--- src/main/java/com/github/phalexei/sig/Main.java
+++ src/main/java/com/github/phalexei/sig/Main.java
@@ -1,44 +1,46 @@
 package com.github.phalexei.sig;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import com.github.phalexei.sig.database.Utils;
 
 public class Main {
-	
-	public Main(){
-		callSQLQuery();
+
+	public Main() {
+		//TODO
+		callSQLQueryQ9();
+		
+		//GeoMainFrame frame = new GeoMainFrame("frame",new MapPanel(100, 100, 100));
 	}
-	
-	
-    public static void main(String[] args) {
-    	new Main();
-    }
-    
-    /**
-     * Simple query
-     */
-    private void callSQLQuery(){
-    	
-    	Connection db = Utils.getConnection();
-    	
-    	try 
-        {
-          Statement st = db.createStatement();
-          ResultSet rs = st.executeQuery("SELECT * FROM users");
-          while ( rs.next() )
-          {
-            System.out.println(rs.toString());
-          }
-          rs.close();
-          st.close();
-        }
-        catch (SQLException se) {
-          System.err.println("Threw a SQLException creating the list of blogs.");
-          System.err.println(se.getMessage());
-        }
-    }
+
+	public static void main(String[] args) {
+		new Main();
+	}
+
+	/**
+	 * Simple query
+	 */
+	private void callSQLQueryQ9() {
+
+		Connection conn = Utils.getConnection();
+
+		try {
+
+			PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users");
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				System.out.println(rs.toString()); 
+				//System.out.println("colonne 1 = " + rs.getInt(1) + "; colonne 2 = " + ((PGgeometry) rs.getObject(2)).getGeometry());
+			}
+			rs.close();
+			stmt.close();
+		} catch (SQLException se) {
+			System.err
+					.println("Threw a SQLException creating the list of blogs.");
+			System.err.println(se.getMessage());
+		}
+	}
 }
