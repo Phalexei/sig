68aab50bdeddee36b5cb83562bff6f0c0ab8552a
alexis <alexis.chretienne@gmail.com>
2015-01-15 13:18:06 +0100

    database connection works

--- src/main/java/com/github/phalexei/sig/Main.java
+++ src/main/java/com/github/phalexei/sig/Main.java
@@ -1,7 +1,44 @@
 package com.github.phalexei.sig;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import sig.Database;
+
 public class Main {
+	
+	public Main(){
+		callSQLQuery();
+	}
+	
+	
     public static void main(String[] args) {
-        System.out.println("Hello wrold!");
+    	new Main();
+    }
+    
+    /**
+     * Simple query
+     */
+    private void callSQLQuery(){
+    	
+    	Connection db = Database.getConnection();
+    	
+    	try 
+        {
+          Statement st = db.createStatement();
+          ResultSet rs = st.executeQuery("SELECT * FROM users");
+          while ( rs.next() )
+          {
+            System.out.println(rs.toString());
+          }
+          rs.close();
+          st.close();
+        }
+        catch (SQLException se) {
+          System.err.println("Threw a SQLException creating the list of blogs.");
+          System.err.println(se.getMessage());
+        }
     }
 }
--- src/main/java/sig/Database.java
+++ src/main/java/sig/Database.java
@@ -1,26 +1,83 @@
 package sig;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Database class. Manage database connection
+ * @author chretiea
+ *
+ */
 public class Database {
 	
-	private final static String JDBC_DRIVER = "";
-	private final static String DB_URL ="jdbc:postgresql://";
+	/**
+	 * Database's informations for connection
+	 */
+	private final static String JDBC_DRIVER = "org.postgresql.Driver";
+	private final static String DB_URL ="jdbc:postgresql://ensibd.imag.fr:5432/osm";
+	private final static String USER = "chretiea";
+	private final static String PASSWORD = "chretiea";
 	
-	private final static String USER = "";
-	private final static String PASSWORD = "";
-	
-	
+	/**
+	 * Variable
+	 */
 	private static Database instance = null;
+	private static Connection connection = null;
 	
+	/**
+	 * Private constructor
+	 */
 	private Database(){
-		super();
+		if(DriverAvailable()){
+			connect();
+		}
+		
 	}
 	
-	public static Database getInstance(){
+	/**
+	 * Connection to the database
+	 */
+	private void connect() {
+		try {
+			 
+			connection = DriverManager.getConnection(DB_URL, USER,PASSWORD);
+			System.out.println("Connection succeed");
+		} catch (SQLException e) {
+ 
+			System.out.println("Connection Failed");
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Check if JDBC driver id found
+	 * @return
+	 */
+	private boolean DriverAvailable(){
+		
+		boolean available = true;
+		try{
+			Class.forName(JDBC_DRIVER);
+		}catch (ClassNotFoundException e) {
+			available = false;
+			System.out.println("Driver not found");
+			e.printStackTrace();
+		}
+		
+		return available;
+	}
+	
+	/**
+	 * Get the database's connection
+	 * @return
+	 */
+	public static Connection getConnection(){
 		if ( instance == null ){
 			instance = new Database();
 		}
 		
-		return instance;
+		return connection;
 	}
 
 }
