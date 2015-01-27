61ebb3f611c0452c14367cccb6704320c962f51d
alexis <alexis.chretienne@gmail.com>
2015-01-27 19:48:12 +0100

    get 3 expected result for Question 9. It misses latitude and longitude

--- src/main/java/com/github/phalexei/sig/Main.java
+++ src/main/java/com/github/phalexei/sig/Main.java
@@ -4,36 +4,50 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import org.postgis.PGgeometry;
-
 import com.github.phalexei.sig.database.Utils;
 
 public class Main {
 
-	public Main() {
-		//TODO
-		callSQLQueryQ9();
+	public Main(String arg0) {
 		
+		//3 rows expected. 3 row found. It misses Latitude and Longitude
+		if (!arg0.isEmpty()){
+			callSQLQueryQ9(arg0);
+		}
+		
+		// TODO : after question 9.
 		//GeoMainFrame frame = new GeoMainFrame("frame",new MapPanel(100, 100, 100));
 	}
 
 	public static void main(String[] args) {
-		new Main();
+		String s = parseArguments(args);
+		new Main(s);
 	}
 
 	/**
-	 * Simple query
+	 * Question 9
 	 */
-	private void callSQLQueryQ9() {
-		//TODO : 3 rows expected. 1 row found !
+	private void callSQLQueryQ9(String arg0) {
+		
+		// Get DB connection
 		Connection conn = Utils.getConnection();
-
+		
+		//make SQL request
+		//TODO : Add Latitude and Longitude in the SQL request
+		String request = "select tags->'name' as nom from nodes where tags->'name' like ? || '%'";
+		
 		try {
-			PreparedStatement stmt = conn.prepareStatement("select tags->'name' as nom, ST_X(ST_Centroid(bbox)) as longitude, ST_Y(ST_Centroid(bbox)) as relations from ways where tags->'name' like '%Domaine Unive%'");
+			//prepare statement
+			PreparedStatement stmt = conn.prepareStatement(request);
+			//add string
+			stmt.setString(1, arg0);
+			//display request
+			System.out.println(stmt.toString());
+			//execute request
 			ResultSet rs = stmt.executeQuery();
+			//display result
 			while (rs.next()) { 
-				System.out.println("nom = " + rs.getString(1) + "; longitude = " + rs.getDouble(2) +" ;  latitude ="+ rs.getDouble(3));
+				System.out.println("nom = " + rs.getString(1));
 			}
 			rs.close();
 			stmt.close();
@@ -42,5 +56,16 @@ public class Main {
 					.println("Threw a SQLException creating the list of blogs.");
 			System.err.println(se.getMessage());
 		}
+	}
+	
+	public static String parseArguments(String[] args){
+		String st = "";
+		for(int i = 0; i<args.length; i++){
+			System.out.println(args[i]);
+			st = st.concat(args[i]);
+			st = st.concat(" ");
+		}
+		st = st.substring(0, st.length() -1);
+		return st;
 	}
 }
