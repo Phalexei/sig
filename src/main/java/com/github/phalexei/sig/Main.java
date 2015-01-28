94e9526af092356fd9ab178fcf52b939276bc16f
alexis <alexis.chretienne@gmail.com>
2015-01-28 16:23:48 +0100

    question 10 B ok

--- src/main/java/com/github/phalexei/sig/Main.java
+++ src/main/java/com/github/phalexei/sig/Main.java
@@ -18,8 +18,9 @@ public class Main {
             question9(arg);
         }*/
 
-        // TODO : other questions using UI
-        question10();
+        //question10_A();
+        
+        //question10_B();
     }
 
     public static void main(String[] args) {
@@ -71,7 +72,7 @@ public class Main {
     /**
      * Question 10
      */
-    private void question10() {
+    private void question10_A() {
         // Get DB connection
         Connection connection = Utils.getConnection();
         try {
@@ -94,9 +95,8 @@ public class Main {
                 for (int i = 0; i < lineString.getGeometry().numPoints() - 1; i++) {
                     Point point = lineString.getGeometry().getPoint(i);
                     guiLineString.addPoint(new com.github.phalexei.sig.gui.Point(point.getX(), point.getY()));
-                    System.out.print("point.getX() = " + point.getX());
-                    System.out.println(":point.getY() = " + point.getY());
                 }
+               
                 panel.addPrimitive(guiLineString);
             }
             resultSet.close();
@@ -111,7 +111,41 @@ public class Main {
     /**
      * Question 11
      */
-    private void question11() {
+    private void question10_B() {
 
+    	// Get DB connection
+        Connection connection = Utils.getConnection();
+        try {
+            //prepare statement
+            Statement statement = connection.createStatement();
+
+            //execute request
+            ResultSet resultSet = statement.executeQuery("SELECT ST_Transform(linestring, 2154) from ways where ST_Intersects(ST_SetSRID" +
+                    "(ST_MakeBox2D(ST_Point(5.7, 45.1), ST_Point(5.8, 45.2)), 4326), linestring)" +
+                    "AND tags ? 'building'");
+
+            MapPanel panel = new MapPanel(919000, 6450000, 1000);
+            Random random = new Random();
+            //display result
+            while (resultSet.next()) {
+                org.postgis.PGgeometry lineString = ((org.postgis.PGgeometry) resultSet.getObject(1));
+
+                LineString guiLineString = new LineString(new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
+                for (int i = 0; i < lineString.getGeometry().numPoints() - 1; i++) {
+                    Point point = lineString.getGeometry().getPoint(i);
+                    guiLineString.addPoint(new com.github.phalexei.sig.gui.Point(point.getX(), point.getY()));
+                }
+                com.github.phalexei.sig.gui.Point first = new com.github.phalexei.sig.gui.Point(lineString.getGeometry().getPoint(0).getX(), lineString.getGeometry().getPoint(0).getY());
+                guiLineString.addPoint(first);
+                panel.addPrimitive(guiLineString);
+            }
+            resultSet.close();
+            statement.close();
+            new GeoMainFrame("frame", panel);
+        } catch (SQLException se) {
+            System.err.println("Threw a SQLException creating the list of blogs.");
+            System.err.println(se.getMessage());
+        }
+    	
     }
 }
