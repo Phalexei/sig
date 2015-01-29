8433f861e71a61bbd1120fb819f6672846596b53
Chloe Mathieu <mathiech@ensipc125.imag.fr>
2015-01-29 11:01:16 +0100

    Essai question 11 C

--- src/main/java/com/github/phalexei/sig/Main.java
+++ src/main/java/com/github/phalexei/sig/Main.java
@@ -43,9 +43,12 @@ public class Main {
         	new Main().question10_C();
         }else if (args[0].equals("11") && args[1].equals("A")){
         	new Main().question11_A();
-        }else if (args[0].equals("10") && args[1].equals("B")){
+        }else if (args[0].equals("11") && args[1].equals("B")){
         	new Main().question11_B();
-        }else{
+        }else if (args[0].equals("11") && args[1].equals("C")){
+        	new Main().question11_C();
+        }
+        else{
         	System.out.println("Mauvais arguments");
         	return;
         }
@@ -280,6 +283,50 @@ public class Main {
 		drawNoisyArea("aeroway", "aerodrome", Color.WHITE, Color.BLACK, panel);
 		//display result
 		new GeoMainFrame("Question 11b", panel);
+
+    }
+    
+    /**
+     * Question 11 C
+     */
+    private void question11_C() {
+
+        // Get DB connection
+        Connection connection = Utils.getConnection();
+        try {
+            //prepare statement
+            Statement statement = connection.createStatement();
+
+            //execute request
+            ResultSet resultSet = statement.executeQuery("SELECT ST_Transform(linestring, 27563) from ways " +
+                    "where tags ? 'building' order by ST_YMin(linestring), St_XMin(linestring) limit 10;");
+
+            MapPanel panel = new MapPanel(919000, 6450000, 1000);
+            Random random = new Random();
+            //get result
+            while (resultSet.next()) {
+//                org.postgis.PGgeometry lineString = ((org.postgis.PGgeometry) resultSet.getObject(1));
+//
+//                LineString guiLineString = new LineString(new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
+//                for (int i = 0; i < lineString.getGeometry().numPoints() - 1; i++) {
+//                    Point point = lineString.getGeometry().getPoint(i);
+//                    guiLineString.addPoint(new com.github.phalexei.sig.gui.Point(point.getX(), point.getY()));
+//                }
+//                com.github.phalexei.sig.gui.Point first = new com.github.phalexei.sig.gui.Point(lineString.getGeometry().getPoint(0).getX(), lineString.getGeometry().getPoint(0).getY());
+//                guiLineString.addPoint(first);
+//                panel.addPrimitive(guiLineString);
+                System.out.println(resultSet.getObject(1).toString());
+            }
+            resultSet.close();
+            statement.close();
+            
+            //display result
+//            new GeoMainFrame("frame", panel);
+        
+        } catch (SQLException se) {
+            System.err.println("Threw a SQLException creating the list of blogs.");
+            System.err.println(se.getMessage());
+        }
 
     }
 
